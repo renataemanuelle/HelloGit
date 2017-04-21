c8a6e91c3541b117b0cb141ad4d3351921a15357
Betty Ribeiro <bettyrib@globo.com>
2017-04-20 22:13:46 -0300

    Betty alterou

--- MeuPrimeiroGit/src/meuprimeirogit/MeuPrimeiroGit.java
+++ MeuPrimeiroGit/src/meuprimeirogit/MeuPrimeiroGit.java
@@ -11,7 +11,7 @@ public class MeuPrimeiroGit {
      */
     public static void main(String[] args) {
         System.out.println("Ola Git!");
-        System.out.println("Modifiquei isso aqui");
+        System.out.println("Betty mexeu aqui");
     }
     
 }

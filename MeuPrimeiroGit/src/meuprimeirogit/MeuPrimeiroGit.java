64f3352f42981112704bd33645030f66b3298cf4
Betty Ribeiro <bettyrib@globo.com>
2017-04-20 22:09:02 -0300

    Nova Alteracao

--- MeuPrimeiroGit/src/meuprimeirogit/MeuPrimeiroGit.java
+++ MeuPrimeiroGit/src/meuprimeirogit/MeuPrimeiroGit.java
@@ -11,7 +11,7 @@ public class MeuPrimeiroGit {
      */
     public static void main(String[] args) {
         System.out.println("Ola Git!");
-        System.out.println("Alterando o codigo");
+        System.out.println("Modifiquei isso aqui");
     }
     
 }
